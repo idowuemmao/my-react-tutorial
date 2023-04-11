a04eae28b2fe9d39f75c26ca0230b93532e6845c
idowu emmanuel <idowuemmao@gmail.com>
2023-04-11 07:35:31 +0100

    Working on the Airbnb project

--- src/components/Airbnb/Airbnb.js
+++ src/components/Airbnb/Airbnb.js
@@ -1,8 +1,11 @@
 import "./Airbnb.css";
 import AirbnbLogo from "./../../images/airbnb.png";
-import AirbnbPic from "./../../images/800px-Colosseum_in_Rome,_Italy_-_April_2007.jpg";
+import AirbnbPic from "./../../images/Group77.png";
+import image1 from "./../../images/image12.png";
+import star from "./../../images/Redstar.png";
 
 export default function Airbnb() {
+  const stars = <img src={star} alt="star" className="star" />;
   return (
     <div className="body">
       <nav className="navbar">
@@ -11,9 +14,15 @@ export default function Airbnb() {
       </nav>
       <img src={AirbnbPic} alt="Airbnb" className="airbnbpic" />
       <h1 className="title">Online Experiences</h1>
-      <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts-all
+        without leaving home.
+      </p>
       <div>
-        
+        <img src={image1} alt="image1" className="image" />
+        <p>
+          {stars} 5.0 <span>(4) . USA</span>
+        </p>
       </div>
     </div>
   );
