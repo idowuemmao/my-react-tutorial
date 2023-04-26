8076328506a73e7572340362a1ee5d2a09138349
idowu emmanuel <idowuemmao@gmail.com>
2023-04-26 03:49:35 +0100

    Process

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 // import Navbar from './components/ReactFacts/Navbar'
 // import Body from './components/ReactFacts/Body'
 // import Buscard from './components/Buscard/Buscard'
-// import Airbnb from "./components/Airbnb/Airbnb"
-import Jokes from "./components/Jokes/Jokes";
-import JokeData from "./components/Jokes/JokeData";
+import Airbnb from "./components/Airbnb/Airbnb";
+// import Jokes from "./components/Jokes/Jokes";
+// import JokeData from "./components/Jokes/JokeData";
 
 export default function Mypage() {
-  const jokeElement = JokeData.map((joke) => {
-    return (
-      <Jokes key={joke.id} setup={joke.setup} punchline={joke.punchline} />
-    );
-  });
+  // const jokeElement = JokeData.map((joke) => {
+  //   return (
+  //     <Jokes key={joke.id} setup={joke.setup} punchline={joke.punchline} />
+  //   );
+  // });
   return (
     <div>
       {/* <Navbar/> */}
       {/* <Body/> */}
       {/* <Buscard/> */}
-      {/* <Airbnb/> */}
-      {jokeElement}
+      <Airbnb />
+      {/* {jokeElement} */}
     </div>
   );
 }
